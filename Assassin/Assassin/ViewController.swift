25104bebbe719f217a71fc4064356e8218dcb1d8
Michael Sacks <michaelsacks@gmail.com>
2015-09-14 10:40:08 -0600

    Initial Commit

---
+++ Assassin/Assassin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assassin
+//
+//  Created by Michael Sacks on 9/14/15.
+//  Copyright © 2015 Michael Sacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
