821af1b677f1c134294ca1e54ae32d425a0edadc
Michael Sacks <michaelsacks@gmail.com>
2015-09-14 15:16:10 -0600

    Adds Basic Storyboard

--- Assassin/Assassin/Login2ViewController.swift
+++ Assassin/Assassin/Login2ViewController.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Login2ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Assassin/Assassin/PersonNearbyTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PersonNearbyTableViewCell.swift
+//  Assassin
+//
+//  Created by Michael Sacks on 9/14/15.
+//  Copyright © 2015 Michael Sacks. All rights reserved.
+//
+
+import UIKit
+
+class PersonNearbyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
