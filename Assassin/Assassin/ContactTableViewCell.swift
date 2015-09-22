5248234e0ddd6b0fc4ee94318b1eb478d3b652ec
Michael Sacks <michaelsacks@gmail.com>
2015-09-22 15:36:35 -0600

    Adds User Detail Table View

---
+++ Assassin/Assassin/ContactTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ContactTableViewCell.swift
+//  Assassin
+//
+//  Created by Michael Sacks on 9/22/15.
+//  Copyright © 2015 Michael Sacks. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var contactImageView: UIView!
+    @IBOutlet weak var contactLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
